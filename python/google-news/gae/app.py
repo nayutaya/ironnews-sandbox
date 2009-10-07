f034c3116d2919a9144c1e65c634719d9f6a0dac
Yuya Kato <yuyakato@gmail.com>
2009-10-07 13:58:16 +0900

    GAE上でRSSを取得できるかどうか試すためのスクリプトを追加

---
+++ python/google-news/gae/app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+print "Content-Type: text/plain"
+print
+
+print "hello"
+
+import urllib
+import urllib2
+
+def create_params(keyword):
+  return {
+    "hl"    : "ja",
+    "ned"   : "us",
+    "ie"    : "UTF-8",
+    "oe"    : "UTF-8",
+    "output": "rss",
+    "q"     : keyword,
+  }
+
+def create_url(keyword):
+  params = create_params(keyword)
+  url    = "http://news.google.com/news?"
+  url   += urllib.urlencode(params)
+  return url
+
+url = create_url("鉄道")
+print url
+req = urllib2.Request(url = url)
+req.add_header("User-Agent", "ironnews google-news-crawler")
+
+io = urllib2.urlopen(req)
+src = io.read()
+io.close()
+
+print src
