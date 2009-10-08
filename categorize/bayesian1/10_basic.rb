b3879de6773705f50226ba5664d69f048e8d9cf5
Yuya Kato <yuyakato@gmail.com>
2009-10-08 21:10:28 +0900

    水平タブの置換処理を追加

--- categorize/bayesian1/10_basic.rb
+++ categorize/bayesian1/10_basic.rb
@@ -2,8 +2,9 @@
 
 # 全角英数字を半角英数字に置換する
 # 全角記号の一部を半角記号に置換する
-# 行頭と行末の空白と削除する
+# 水平タブを空白に置換する
 # 連続した空白を単一の空白に置換する
+# 行頭と行末の空白と削除する
 
 require "nkf"
 
@@ -11,7 +12,8 @@ STDOUT.binmode
 
 STDIN.each { |line|
   line = NKF.nkf("-W -w80 -m0 -Z1", line)
+  line.gsub!(/\t/, " ")
+  line.gsub!(/ {2,}/, " ")
   line.strip!
-  line.gsub!(/ +/, " ")
   STDOUT.puts(line)
 }
