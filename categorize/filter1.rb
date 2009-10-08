683511bbb4bc2053d999ba34b947aa799dcbac0e
Yuya Kato <yuyakato@gmail.com>
2009-10-08 14:38:54 +0900

    標準入出力を使うように変更

--- categorize/filter1.rb
+++ categorize/filter1.rb
@@ -2,15 +2,12 @@
 
 require "nkf"
 
-def filter(line)
-  #return NKF.nkf("-S -w80 -m0", NKF.nkf("-W8 -s -m0", line))
+def full_to_half(line)
   return NKF.nkf("-W8 -w80 -m0 -Z1", line)
 end
 
-File.open("data/title_rail.txt", "r") { |infile|
-  File.open("out1.txt", "w") { |outfile|
-    infile.each { |line|
-      outfile.write(filter(line))
-    }
-  }
+STDIN.each { |line|
+  line.chomp!
+  line = full_to_half(line)
+  STDOUT.puts(line)
 }
