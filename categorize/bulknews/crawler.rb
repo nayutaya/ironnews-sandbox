53cd3def65e21e9e7c32938dd1213ff87fea50aa
Yuya Kato <yuyakato@gmail.com>
2009-10-09 16:22:44 +0900

    指定した日付の範囲を一括して取得するように変更

--- categorize/bulknews/crawler.rb
+++ categorize/bulknews/crawler.rb
@@ -6,27 +6,29 @@ require "nkf"
 require "rubygems"
 require "nokogiri"
 
-date = Date.new(2009, 5, 1)
-url = "http://bulknews.net/?date=" + date.strftime("%Y%m%d")
-#src = open(url) { |io| io.read }
-#File.open("out.html", "wb") { |file| file.write(src) }
-src = File.open("out.html", "rb") { |file| file.read }
-src = NKF.nkf("-E -w80 -m0", src)
-#p src
+start_date = Date.new(2009, 3, 1)
+end_date   = Date.new(2009, 4, 1) - 1
 
-doc  = Nokogiri(src)
-#body = doc.xpath("/html/body")
+(start_date..end_date).each { |date|
+  STDERR.puts(date.strftime("%Y/%m/%d"))
 
-tables = doc.search("//table[@width='100%']")
-tables.each { |table|
-  title = table.at("tr[1]/td/b/a/font")
-  if title && ["asahi.com", "NIKKEI NET"].include?(title.text)
-    body = table.at("tr[2]/td")
-    puts("---")
-    links = body.search("a[@target='_blank']")
-    links.each { |link|
-      title = link.next_sibling.text.strip
-      puts(title) if title != ">>続き"
-    }
-  end
+  url = "http://bulknews.net/?date=" + date.strftime("%Y%m%d")
+  src = open(url) { |io| io.read }
+  src = NKF.nkf("-E -w80 -m0", src)
+  doc = Nokogiri(src)
+
+  tables = doc.search("//table[@width='100%']")
+  tables.each { |table|
+    title = table.at("tr[1]/td/b/a/font")
+    if title && ["asahi.com", "NIKKEI NET"].include?(title.text)
+      body = table.at("tr[2]/td")
+      links = body.search("a[@target='_blank']")
+      links.each { |link|
+        title = link.next_sibling.text.strip
+        puts(title) if title != ">>続き"
+      }
+    end
+  }
+
+  sleep(1.0)
 }
