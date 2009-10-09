83242961fe6aef362667c3f82972ac2072a74a5c
Yuya Kato <yuyakato@gmail.com>
2009-10-09 16:15:49 +0900

    タイトルの取得処理を追加

--- categorize/bulknews/crawler.rb
+++ categorize/bulknews/crawler.rb
@@ -17,15 +17,16 @@ src = NKF.nkf("-E -w80 -m0", src)
 doc  = Nokogiri(src)
 #body = doc.xpath("/html/body")
 
-tables = doc.xpath("//table[@width='100%']")
-p tables.size
-tables.each { |elem|
-  puts("===")
-  title = elem.at("tr[1]/td[1]/b/a/font")
+tables = doc.search("//table[@width='100%']")
+tables.each { |table|
+  title = table.at("tr[1]/td/b/a/font")
   if title && ["asahi.com", "NIKKEI NET"].include?(title.text)
-    p title
-    p title.text if title
+    body = table.at("tr[2]/td")
     puts("---")
-    puts(elem.to_html)
+    links = body.search("a[@target='_blank']")
+    links.each { |link|
+      title = link.next_sibling.text.strip
+      puts(title) if title != ">>続き"
+    }
   end
 }
