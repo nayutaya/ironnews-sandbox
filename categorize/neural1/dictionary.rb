718e07677f280be72a288ad2f8ecb57f00ce3949
Yuya Kato <yuyakato@gmail.com>
2009-10-16 01:08:22 +0900

    配列とハッシュのエンコード、デコード処理を追加

--- categorize/neural1/dictionary.rb
+++ categorize/neural1/dictionary.rb
@@ -16,8 +16,38 @@ class Dictionary
       end
   end
 
+  def encode_multiple(collection)
+    case collection
+    when Array
+      return collection.map { |obj|
+        self.encode(obj)
+      }
+    when Hash
+      return collection.inject({}) { |memo, (obj, value)|
+        memo[self.encode(obj)] = value
+        memo
+      }
+    else raise(ArgumentError)
+    end
+  end
+
   def decode(id)
     return @id2obj[id]
+  end
+
+  def decode_multiple(collection)
+    case collection
+    when Array
+      return collection.map { |id|
+        self.decode(id)
+      }
+    when Hash
+      return collection.inject({}) { |memo, (id, value)|
+        memo[self.decode(id)] = value
+        memo
+      }
+    else raise(ArgumentError)
+    end
   end
 end
 
@@ -27,7 +57,11 @@ p dict.encode("b")
 p dict.encode("a")
 p dict.encode("b")
 p dict.encode(1)
+p dict.encode_multiple(["a", "b", 1, 2])
+p dict.encode_multiple({"a" => :a, "b" => :b, 1 => :one, 2 => :two})
 
 p dict.decode(1)
 p dict.decode(2)
 p dict.decode(3)
+p dict.decode_multiple([1, 2, 3, 5])
+p dict.decode_multiple({1 => :a, 2 => :b, 3 => :one, 5 => :nil})
