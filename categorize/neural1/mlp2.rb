452ee674066a042142253a2e354158655d64a19d
Yuya Kato <yuyakato@gmail.com>
2009-10-16 15:12:55 +0900

    バックプロパゲーションを実装

--- categorize/neural1/mlp2.rb
+++ categorize/neural1/mlp2.rb
@@ -69,8 +69,72 @@ class ThreeLayerPerceptronNetwork
         weight = @output_hidden_weight[output_id, hidden_id]
         result + value * weight
       }
-      output_values[output_id] = self.sigmoid(sum)
+      output_values[output_id] = @sigmoid[sum]
       output_values
+    }
+  end
+
+  def backpropagation(input_values, target_values)
+    input_values  = input_values.merge(0 => 1.0)
+    hidden_values = self.feedforward_input_to_hidden(input_values)
+    hidden_values = hidden_values.merge(0 => 1.0)
+    output_values = self.feedforward_hidden_to_output(hidden_values)
+
+    output_delta = self.calc_output_delta(target_values, output_values)
+    hidden_delta = self.calc_hidden_delta(output_delta, hidden_values)
+
+    n = 0.5 # 学習率
+    self.update_input_hidden_weight(hidden_delta, input_values, n)
+    self.update_output_hidden_weight(output_delta, hidden_values, n)
+
+    return output_delta.values.inject(0.0) { |sum, delta| sum + (delta * delta) }
+  end
+
+  def calc_output_delta(target_values, output_values)
+    output_ids = target_values.keys
+
+    return output_ids.inject({}) { |output_delta, output_id|
+      target = target_values[output_id] || 0.0
+      output = output_values[output_id] || 0.0
+      error  = target - output
+      output_delta[output_id] = @dsigmoid[output] * error
+      output_delta
+    }
+  end
+
+  def calc_hidden_delta(output_delta, hidden_values)
+    output_ids = output_delta.keys
+
+    return (0..@hidden_num).inject({}) { |hidden_delta, hidden_id|
+      error = output_ids.inject(0.0) { |result, output_id|
+        delta  = output_delta[output_id]
+        weight = @output_hidden_weight[output_id, hidden_id]
+        result + delta * weight
+      }
+      hidden_delta[hidden_id] = @dsigmoid[hidden_values[hidden_id]] * error
+      hidden_delta
+    }
+  end
+
+  def update_input_hidden_weight(hidden_delta, input_values, n)
+    input_values.keys.each { |input_id|
+      (1..@hidden_num).each { |hidden_id|
+        delta  = hidden_delta[hidden_id]
+        value  = input_values[input_id]
+        change = delta * value
+        @input_hidden_weight[input_id, hidden_id] += n * change
+      }
+    }
+  end
+
+  def update_output_hidden_weight(output_delta, hidden_values, n)
+    (0..@hidden_num).each { |hidden_id|
+      @output_hidden_weight.from_keys.each { |output_id|
+        delta  = output_delta[output_id]
+        value  = hidden_values[hidden_id]
+        change = delta * value
+        @output_hidden_weight[output_id, hidden_id] += n * change
+      }
     }
   end
 end
@@ -78,9 +142,21 @@ end
 if $0 == __FILE__
   srand(0)
   network = ThreeLayerPerceptronNetwork.new(3)
+  puts "before:"
   p network.feedforward(1 => 0.0, 2 => 0.0)
   p network.feedforward(1 => 0.0, 2 => 1.0)
   p network.feedforward(1 => 1.0, 2 => 0.0)
   p network.feedforward(1 => 1.0, 2 => 1.0)
-  p network
+
+  puts "train:"
+  p network.backpropagation({1 => 0, 2 => 0}, {1 => 1, 2 => 0})
+  #network.backpropagation({1 => 0, 2 => 1}, {1 => 0, 2 => 1})
+  #network.backpropagation({1 => 1, 2 => 0}, {1 => 0, 2 => 1})
+  #network.backpropagation({1 => 1, 2 => 1}, {1 => 1, 2 => 0})
+
+  puts "after:"
+  p network.feedforward(1 => 0.0, 2 => 0.0)
+  p network.feedforward(1 => 0.0, 2 => 1.0)
+  p network.feedforward(1 => 1.0, 2 => 0.0)
+  p network.feedforward(1 => 1.0, 2 => 1.0)
 end
