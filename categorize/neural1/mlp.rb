7413dbd2911f135ddb34a3e48225bf83c6847243
Yuya Kato <yuyakato@gmail.com>
2009-10-16 12:15:40 +0900

    リファクタリング

--- categorize/neural1/mlp.rb
+++ categorize/neural1/mlp.rb
@@ -30,62 +30,53 @@ class ThreeLayerPerceptronNetwork
     }
   end
 
-  def feedforward_in_to_mid(in_values)
-    mid = {}
-    (1..@mid_num).each { |mid_key|
-      sum = 0.0
-      in_values.each { |in_key, in_value|
-        weight = @in_mid[in_key][mid_key]
-        sum += in_value * weight
-      }
-      mid[mid_key] = Math.tanh(sum)
-    }
+  def sigmoid(x)
+    return Math.tanh(x)
+  end
 
-    return mid
+  def dsigmoid(x)
+    return 1.0 - x * x
+  end
+
+  def feedforward_in_to_mid(in_values)
+    return (1..@mid_num).inject({}) { |mid_values, mid_key|
+      sum = in_values.inject(0.0) { |result, (in_key, in_value)|
+        weight = @in_mid[in_key][mid_key]
+        value  = in_value
+        result + value * weight
+      }
+      mid_values[mid_key] = self.sigmoid(sum)
+      mid_values
+    }
   end
 
   def feedforward_mid_to_out(mid_values)
-    out = {}
-    (1..@out_num).each { |out_key|
-      sum = 0.0
-      (0..@mid_num).each { |mid_key|
+    return (1..@out_num).inject({}) { |out_values, out_key|
+      sum = (0..@mid_num).inject(0.0) { |result, mid_key|
         weight = @mid_out[mid_key][out_key]
         value  = mid_values[mid_key]
-        sum += value * weight
+        result + value * weight
       }
-      out[out_key] = Math.tanh(sum)
+      out_values[out_key] = self.sigmoid(sum)
+      out_values
     }
-
-    return out
   end
 
-  def backpropagation_out_to_mid(ret_values, out)
-    out_delta = {}
-    (1..@out_num).each { |out_key|
-      error = ret_values[out_key] - out[out_key]
-      out_delta[out_key] = self.dtanh(out[out_key]) * error
+  def backpropagation_out_to_mid(target_values, out_values)
+    return (1..@out_num).inject({}) { |out_delta, out_key|
+      error = target_values[out_key] - out_values[out_key]
+      out_delta[out_key] = self.dsigmoid(out_values[out_key]) * error
+      out_delta
     }
-    return out_delta
   end
 
-  def backpropagation_mid_to_in(out_delta, mid)
-    mid_delta = {}
-    (0..@mid_num).each { |mid_key|
-      error = 0.0
-      (1..@out_num).each { |out_key|
-        error += out_delta[out_key] * @mid_out[mid_key][out_key]
+  def backpropagation_mid_to_in(out_delta, mid_values)
+    return (0..@mid_num).inject({}) { |mid_delta, mid_key|
+      error = (1..@out_num).inject(0.0) { |result, out_key|
+        result + out_delta[out_key] * @mid_out[mid_key][out_key]
       }
-      mid_delta[mid_key] = self.dtanh(mid[mid_key]) * error
-    }
-    return mid_delta
-  end
-
-  def update_mid_to_out(out_delta, mid, n)
-    (0..@mid_num).each { |mid_key|
-      (1..@out_num).each { |out_key|
-        change = out_delta[out_key] * mid[mid_key]
-        @mid_out[mid_key][out_key] += n * change
-      }
+      mid_delta[mid_key] = self.dsigmoid(mid_values[mid_key]) * error
+      mid_delta
     }
   end
 
@@ -98,16 +89,21 @@ class ThreeLayerPerceptronNetwork
     }
   end
 
+  def update_mid_to_out(out_delta, mid_values, n)
+    (0..@mid_num).each { |mid_key|
+      (1..@out_num).each { |out_key|
+        change = out_delta[out_key] * mid_values[mid_key]
+        @mid_out[mid_key][out_key] += n * change
+      }
+    }
+  end
+
   def fire(in_values)
     in_values  = in_values.merge(0 => 1.0)
     mid_values = self.feedforward_in_to_mid(in_values)
     mid_values = mid_values.merge(0 => 1.0)
     out_values = self.feedforward_mid_to_out(mid_values)
     return out_values
-  end
-
-  def dtanh(x)
-    return 1.0 - x * x
   end
 
   # バックプロパゲーションによるトレーニング
@@ -121,8 +117,8 @@ class ThreeLayerPerceptronNetwork
     mid_delta = self.backpropagation_mid_to_in(out_delta, mid_values)
 
     n = 0.5 # 学習率
-    self.update_mid_to_out(out_delta, mid_values, n)
     self.update_in_to_mid(mid_delta, in_values, n)
+    self.update_mid_to_out(out_delta, mid_values, n)
 
     return out_delta.values.inject(0.0) { |sum, val| sum + val * val }
   end
