6d8d721730415202929127e44cd24e075a9a70aa
Yuya Kato <yuyakato@gmail.com>
2009-10-28 11:24:26 +0900

    ログイン、ログアウトアクションを追加

--- rails/wsse1/app/controllers/home_controller.rb
+++ rails/wsse1/app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
+
 class HomeController < ApplicationController
   def index
   end
 
+  def login
+    flash[:notice] = "ログインしました。"
+    redirect_to(:action => "index")
+  end
+
+  def logout
+    flash[:notice] = "ログアウトしました。"
+    redirect_to(:action => "index")
+  end
 end
