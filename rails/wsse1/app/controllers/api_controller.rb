8c1e9b6a80707d43161f20155cd350e6910ebad4
Yuya Kato <yuyakato@gmail.com>
2009-10-28 13:07:09 +0900

    WSSEによる認証処理を追加

--- rails/wsse1/app/controllers/api_controller.rb
+++ rails/wsse1/app/controllers/api_controller.rb
@@ -1,4 +1,7 @@
 
+require "wsse/username_token_parser" # nayutaya-wsse
+require "wsse/username_token_builder" # nayutaya-wsse
+
 class ApiController < ApplicationController
   before_filter :authentication, :only => [:go]
 
@@ -11,14 +14,28 @@ class ApiController < ApplicationController
   def authentication
     user_id = session[:user_id]
     wsse    = request.env["HTTP_X_WSSE"]
-p wsse
-#p request.user_agent
-#p request.env
 
     if !user_id.blank?
       return true
     elsif !wsse.blank?
-      return true
+      username = "foo"
+      password = "bar"
+
+      parsed_token = Wsse::UsernameTokenParser.parse(wsse)
+      wsse_username = parsed_token["Username"]
+      wsse_nonce    = parsed_token["Nonce"]
+      wsse_digest   = parsed_token["PasswordDigest"]
+      wsse_created  = parsed_token["Created"]
+
+      # FIXME: createdの範囲を限定
+      # FIXME: リピート攻撃に対処
+
+      generated_token = Wsse::UsernameTokenBuilder.create_token_params(username, password, wsse_nonce.unpack("m")[0], wsse_created)
+      digest = generated_token["PasswordDigest"]
+
+      if wsse_username == username && wsse_digest == digest
+        return true
+      end
     end
 
     render(:text => "authentication failed")
--- rails/wsse1/wsse_test.rb
+++ rails/wsse1/wsse_test.rb
@@ -1,7 +1,15 @@
 #! ruby -Ku
 
 require "open-uri"
+require "rubygems"
+gem "nayutaya-wsse"
+require "wsse/username_token_builder"
 
-open("http://10.161.227.115:3000/api/go", {"X-WSSE" => "hoge"}) { |io|
+username = "foo"
+password = "bar"
+wsse     = Wsse::UsernameTokenBuilder.create_token(username, password)
+p wsse
+
+open("http://10.161.227.115:3000/api/go", {"X-WSSE" => wsse}) { |io|
   p io.read
 }
