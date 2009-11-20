c0bd5af98611338e23d5655cdf8cb057713b8cd4
Yuya Kato <yuyakato@gmail.com>
2009-11-20 15:40:28 +0900

    URLの標準化処理を追加

--- ruby/canonical-url/bookmark_util.rb
+++ ruby/canonical-url/bookmark_util.rb
@@ -3,6 +3,10 @@ module BookmarkUtil
   def self.get_canonical_url(url)
     url = url.sub(/\A(http:\/\/journal\.mycom\.co\.jp\/.+)\?rt=na\z/, '\1index.html')
     url = url.sub(/\A(http:\/\/mainichi\.jp\/.+)\?link_id=[A-Z0-9]+\z/, '\1')
+    url = url.sub(/\A(http:\/\/news\.searchina\.ne\.jp\/.+)&pt=large\z/, '\1')
+    url = url.sub(/\A(http:\/\/osaka\.yomiuri\.co\.jp\/.+)\?from=[a-z0-9]+\z/, '\1')
+    url = url.sub(/\A(http:\/\/www\.asahi\.com\/.+)\?ref=rss\z/, '\1')
+    url = url.sub(/\A(http:\/\/www\.tetsudo\.com\/.+)\?tag=as\.rss/, '\1')
     return url
   end
 end
--- ruby/canonical-url/bookmark_util_test.rb
+++ ruby/canonical-url/bookmark_util_test.rb
@@ -16,6 +16,16 @@ class BookmarkUtilTest < Test::Unit::TestCase
       ["http://journal.mycom.co.jp/series/photograph/031/?rt=na", "http://journal.mycom.co.jp/series/photograph/031/index.html"],
       ["http://mainichi.jp/select/today/news/20091109k0000e040037000c.html?link_id=RSH02", "http://mainichi.jp/select/today/news/20091109k0000e040037000c.html"],
       ["http://mainichi.jp/select/today/news/20091117k0000m040060000c.html?link_id=RTH05", "http://mainichi.jp/select/today/news/20091117k0000m040060000c.html"],
+      ["http://news.searchina.ne.jp/disp.cgi?y=2009&d=1108&f=national_1108_006.shtml&pt=large", "http://news.searchina.ne.jp/disp.cgi?y=2009&d=1108&f=national_1108_006.shtml"],
+      ["http://news.searchina.ne.jp/disp.cgi?y=2009&d=1112&f=national_1112_004.shtml&pt=large", "http://news.searchina.ne.jp/disp.cgi?y=2009&d=1112&f=national_1112_004.shtml"],
+      ["http://osaka.yomiuri.co.jp/ekiben/eb91118a.htm?from=ichioshi", "http://osaka.yomiuri.co.jp/ekiben/eb91118a.htm"],
+      ["http://osaka.yomiuri.co.jp/nara/news/20091105-OYO8T00343.htm?from=ichioshi", "http://osaka.yomiuri.co.jp/nara/news/20091105-OYO8T00343.htm"],
+      ["http://osaka.yomiuri.co.jp/news/20091110-OYO1T00789.htm?from=top", "http://osaka.yomiuri.co.jp/news/20091110-OYO1T00789.htm"],
+      ["http://osaka.yomiuri.co.jp/news/20091116-OYO1T00291.htm?from=main1", "http://osaka.yomiuri.co.jp/news/20091116-OYO1T00291.htm"],
+      ["http://www.asahi.com/national/update/1120/SEB200911200005.html?ref=rss", "http://www.asahi.com/national/update/1120/SEB200911200005.html"],
+      ["http://www.asahi.com/sports/update/1120/TKY200911200100.html?ref=rss",   "http://www.asahi.com/sports/update/1120/TKY200911200100.html"],
+      ["http://www.tetsudo.com/news/466/%E6%97%A5%E5%90%91%E5%B8%82%E9%A7%85%E3%81%8C%E5%BB%BA%E7%AF%89%E6%A5%AD%E5%8D%94%E4%BC%9A%E8%B3%9E%E3%82%92%E5%8F%97%E8%B3%9E/?tag=as.rss", "http://www.tetsudo.com/news/466/%E6%97%A5%E5%90%91%E5%B8%82%E9%A7%85%E3%81%8C%E5%BB%BA%E7%AF%89%E6%A5%AD%E5%8D%94%E4%BC%9A%E8%B3%9E%E3%82%92%E5%8F%97%E8%B3%9E/"],
+      ["http://www.tetsudo.com/news/467/%E6%9D%B1%E6%80%A5%E7%94%B0%E5%9C%92%E9%83%BD%E5%B8%82%E7%B7%9A%E3%81%A7%E6%97%A9%E8%B5%B7%E3%81%8D%E5%BF%9C%E6%8F%B4%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%9A%E3%83%BC%E3%83%B3/?tag=as.rss", "http://www.tetsudo.com/news/467/%E6%9D%B1%E6%80%A5%E7%94%B0%E5%9C%92%E9%83%BD%E5%B8%82%E7%B7%9A%E3%81%A7%E6%97%A9%E8%B5%B7%E3%81%8D%E5%BF%9C%E6%8F%B4%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%9A%E3%83%BC%E3%83%B3/"],
     ].each { |value, expected|
       assert_equal(expected, @module.get_canonical_url(value), value)
     }
