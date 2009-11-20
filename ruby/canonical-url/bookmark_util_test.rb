bf52aed8388fc73f83733631fc6d37b307558daa
Yuya Kato <yuyakato@gmail.com>
2009-11-20 15:17:40 +0900

    ブックマークに関するユーティリティの雛形を追加

---
+++ ruby/canonical-url/bookmark_util_test.rb
@@ -0,0 +1,15 @@
+#! ruby -Ku
+
+require "test/unit"
+require "bookmark_util"
+
+class BookmarkUtilTest < Test::Unit::TestCase
+  def setup
+    @module = BookmarkUtil
+  end
+
+def test_ok
+assert true
+end
+end
+
