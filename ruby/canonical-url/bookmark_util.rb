f5e4ca3528ec9dd2f7aa2427d4004acfcd7bf278
Yuya Kato <yuyakato@gmail.com>
2009-11-20 15:51:50 +0900

    リファクタリング
    * パターン、テストデータをソート

--- ruby/canonical-url/bookmark_util.rb
+++ ruby/canonical-url/bookmark_util.rb
@@ -1,14 +1,14 @@
 
 module BookmarkUtil
   def self.get_canonical_url(url)
+    url = url.sub(/\A(http:\/\/[a-z]+\.yomiuri\.co\.jp\/.+)\?from=[a-z0-9]+\z/, '\1')
     url = url.sub(/\A(http:\/\/journal\.mycom\.co\.jp\/.+)\?rt=na\z/, '\1index.html')
     url = url.sub(/\A(http:\/\/mainichi\.jp\/.+)\?link_id=[A-Z0-9]+\z/, '\1')
     url = url.sub(/\A(http:\/\/news\.searchina\.ne\.jp\/.+)&pt=large\z/, '\1')
-    url = url.sub(/\A(http:\/\/[a-z]+\.yomiuri\.co\.jp\/.+)\?from=[a-z0-9]+\z/, '\1')
     url = url.sub(/\A(http:\/\/www\.asahi\.com\/.+)\?ref=rss\z/, '\1')
-    url = url.sub(/\A(http:\/\/www\.tetsudo\.com\/.+)\?tag=as\.rss/, '\1')
     url = url.sub(/\A(http:\/\/www\.chunichi\.co\.jp\/.+)\?ref=rank\z/, '\1')
     url = url.sub(/\A(http:\/\/www\.jiji\.com\/jc\/c\?g=.+?)&rel=j7(&k=\d+)\z/, '\1\2')
+    url = url.sub(/\A(http:\/\/www\.tetsudo\.com\/.+)\?tag=as\.rss/, '\1')
     return url
   end
 end
