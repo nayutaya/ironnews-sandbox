b32cf7be5a170799ceb65d95887c41badd94eca9
Yuya Kato <yuyakato@gmail.com>
2009-11-20 15:28:53 +0900

    URLの標準化処理を追加

--- ruby/canonical-url/bookmark_util.rb
+++ ruby/canonical-url/bookmark_util.rb
@@ -1,3 +1,8 @@
 
 module BookmarkUtil
+  def self.get_canonical_url(url)
+    url = url.sub(/\A(http:\/\/journal\.mycom\.co\.jp\/.+)\?rt=na\z/, '\1index.html')
+    url = url.sub(/\A(http:\/\/mainichi\.jp\/.+)\?link_id=[A-Z0-9]+\z/, '\1')
+    return url
+  end
 end
--- ruby/canonical-url/bookmark_util_test.rb
+++ ruby/canonical-url/bookmark_util_test.rb
@@ -8,8 +8,16 @@ class BookmarkUtilTest < Test::Unit::TestCase
     @module = BookmarkUtil
   end
 
-def test_ok
-assert true
-end
-end
+  def test_get_canonical_url
+    [
+      ["http://www.asahi.com/", "http://www.asahi.com/"],
 
+      ["http://journal.mycom.co.jp/news/2009/11/09/027/?rt=na",   "http://journal.mycom.co.jp/news/2009/11/09/027/index.html"],
+      ["http://journal.mycom.co.jp/series/photograph/031/?rt=na", "http://journal.mycom.co.jp/series/photograph/031/index.html"],
+      ["http://mainichi.jp/select/today/news/20091109k0000e040037000c.html?link_id=RSH02", "http://mainichi.jp/select/today/news/20091109k0000e040037000c.html"],
+      ["http://mainichi.jp/select/today/news/20091117k0000m040060000c.html?link_id=RTH05", "http://mainichi.jp/select/today/news/20091117k0000m040060000c.html"],
+    ].each { |value, expected|
+      assert_equal(expected, @module.get_canonical_url(value), value)
+    }
+  end
+end
