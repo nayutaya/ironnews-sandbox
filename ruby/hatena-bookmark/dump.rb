ae6f8e765c75894697fafbad605628b09d7fdcb6
Yuya Kato <yuyakato@gmail.com>
2009-12-08 13:39:48 +0900

    はてなブックマークの情報をダンプするスクリプトを追加

---
+++ ruby/hatena-bookmark/dump.rb
@@ -0,0 +1,23 @@
+#! ruby -Ku
+
+require "net/http"
+require "uri"
+require "rubygems"
+require "wsse"
+
+username, password = File.open("hatena.id") { |file| [file.gets.strip, file.gets.strip] }
+wsse = Wsse::UsernameToken.build(username, password).format
+
+uri = URI.parse("http://b.hatena.ne.jp/dump")
+
+res = Net::HTTP.start(uri.host, uri.port) {|http|
+  http.open_timeout = 5
+  http.read_timeout = 60
+  http.get(uri.path, {"X-WSSE" => wsse})
+}
+
+xml = res.body
+
+File.open("dump.xml", "wb") { |file|
+  file.write(xml)
+}
