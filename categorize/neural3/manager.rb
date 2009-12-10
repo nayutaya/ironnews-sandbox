7f5361e40a6e3191e19eecff438d2e1ea02a58ec
Yuya Kato <yuyakato@gmail.com>
2009-12-10 15:30:20 +0900

    URIをバッチファイルとして出力する処理を追加

--- categorize/neural3/manager.rb
+++ categorize/neural3/manager.rb
@@ -7,6 +7,10 @@ ts = Rinda::TupleSpace.new
 DRb.start_service(nil, ts)
 puts DRb.uri
 
+File.open("uri.bat", "w") { |file|
+  file.printf("@set URI=%s\n", DRb.uri)
+}
+
 loop {
   symbol, result = ts.take([:result, Hash])
   p result
