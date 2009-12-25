52d60db743d58d101c95da0302248a19e483b0f6
Yuya Kato <yuyakato@gmail.com>
2009-12-25 16:16:34 +0900

    クロステストに用いるデータを変更

--- categorize/bayesian3/crosstest3.bat
+++ categorize/bayesian3/crosstest3.bat
@@ -3,20 +3,22 @@ set DBNAME=crosstest3.db
 set THRESHOLDS=rail 1.0 rest 3.5
 
 del %DBNAME%
-
-type ..\..\..\ironnews-data\news_title\rail_*.txt > rail.txt
-type ..\..\..\ironnews-data\news_title\rest_*.txt > rest.txt
-ruby train.rb %DBNAME% rail < rail.txt
-ruby train.rb %DBNAME% rest < rest.txt
 del rail.txt
 del rest.txt
 
-ruby train.rb %DBNAME% rail < ..\..\..\ironnews-data\news_url_title\rail_01.txt
-ruby train.rb %DBNAME% rest < ..\..\..\ironnews-data\news_url_title\rest_01.txt
+type ..\..\..\ironnews-data\news_url_title\rail.txt >> rail.txt
+type ..\..\..\ironnews-data\news_url_title\rest.txt >> rest.txt
+type ..\..\..\ironnews-data\news_title\rail_*.txt >> rail.txt
+type ..\..\..\ironnews-data\news_title\rest_*.txt >> rest.txt
 
-ruby crosstest.rb %DBNAME% rail %THRESHOLDS% < ..\..\..\ironnews-data\news_url_title\rail_01.txt  > crosstest3.txt
-ruby crosstest.rb %DBNAME% rest %THRESHOLDS% < ..\..\..\ironnews-data\news_url_title\rest_01.txt >> crosstest3.txt
+ruby train.rb %DBNAME% rail < rail.txt
+ruby train.rb %DBNAME% rest < rest.txt
+
+ruby crosstest.rb %DBNAME% rail %THRESHOLDS% < ..\..\..\ironnews-data\news_url_title\rail.txt  > crosstest3.txt
+ruby crosstest.rb %DBNAME% rest %THRESHOLDS% < ..\..\..\ironnews-data\news_url_title\rest.txt >> crosstest3.txt
 
 del %DBNAME%
+del rail.txt
+del rest.txt
 
 pause
